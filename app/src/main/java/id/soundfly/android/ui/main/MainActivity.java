c1c4fb74c20814868436245140b8cefe6261f43c
fsoeltoni <fsoeltoni@gmail.com>
2018-10-01 03:23:34 +0700

    Configure butterknife

--- app/src/main/java/id/soundfly/android/ui/main/MainActivity.java
+++ app/src/main/java/id/soundfly/android/ui/main/MainActivity.java
@@ -1,29 +1,30 @@
 package id.soundfly.android.ui.main;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import butterknife.BindView;
+import butterknife.OnClick;
 import id.soundfly.android.R;
 import id.soundfly.android.ui.utils.BaseActivity;
 
 public class MainActivity extends BaseActivity {
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(this::onClickListenerFab);
     }
 
+    @OnClick(R.id.fab)
     void onClickListenerFab(View view){
         Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null)
--- app/src/main/java/id/soundfly/android/ui/utils/BaseActivity.java
+++ app/src/main/java/id/soundfly/android/ui/utils/BaseActivity.java
@@ -1,6 +1,24 @@
 package id.soundfly.android.ui.utils;
 
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import dagger.android.support.DaggerAppCompatActivity;
 
 public abstract class BaseActivity extends DaggerAppCompatActivity {
+
+    private Unbinder unbinder;
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        unbinder = ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (unbinder != null) {
+            unbinder.unbind();
+        }
+    }
 }
